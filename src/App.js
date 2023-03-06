4b41769fa75be0ca12b35239305bcd8732b97db9
PabloKauan-TI <kpablo429@gmail.com>
2023-03-06 18:03:15 -0300

    Criando container central e seus css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Container from './components/Container';
 import Home from './pages/Home';
 import Company from './pages/Company';
 import Contact from './pages/Contact';
@@ -7,12 +8,20 @@ import NewProject from './pages/NewProject';
 function App() {
   return (
    <Router>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/company" element={<Company />} />
-      <Route exact path="/contact" element={<Contact />} />
-      <Route exact path="/newproject" element={<NewProject />} />
-    </Routes>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/company">Company</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/newproject">New Project</Link>
+    </div>
+    <Container customClass='minHeight'>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/company" element={<Company />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/newproject" element={<NewProject />} />
+      </Routes>
+    </Container>
     <p>Footer</p>
    </Router>
   );
