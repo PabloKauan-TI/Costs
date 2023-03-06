5cfbd786ff8655318db99162b5e6dd2409f314a9
PabloKauan-TI <kpablo429@gmail.com>
2023-03-06 16:53:31 -0300

    Criação das Rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Company from './pages/Company';
+import Contact from './pages/Contact';
+import NewProject from './pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+   <Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/company" element={<Company />} />
+      <Route exact path="/contact" element={<Contact />} />
+      <Route exact path="/newproject" element={<NewProject />} />
+    </Routes>
+    <p>Footer</p>
+   </Router>
   );
 }
 
